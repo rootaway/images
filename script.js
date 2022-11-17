73fbfd95d49f8e7250f12c1c99475f53432f8e3c
Andrey Strogin <andrey.strogin89@gmail.com>
2022-11-17 15:53:46 +0300

    8

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ function ex(e){
 
 link.addEventListener('click', ex);
 X.addEventListener('click', ex);
+
+const nodeImage = document.querySelectorAll('.image > img');
+const array = Array.from(nodeImage);
+
+array.forEach((el,i) => {
+    el.classList.add(`img-${i+1}`)
+});
